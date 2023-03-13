08ec8bc29ef511718191f06ef8ef2a51d760daba
DavidYeLuo <davidayeluo@gmail.com>
2023-03-12 18:19:02 -0700

    Added a program that can display a grayscaled image to a 3D mesh. It also includes a histogram of the image.

--- Main.m
+++ Main.m
@@ -1,35 +1,3 @@
-%v = VideoReader("X265-Crf15-1.mp4");
-%n = 0;
-%number_of_frames = 2;
-%{
-while n < 2
-    frame = readFrame(v);
-    %gray = rgb2gray(frame);
-    imshow(frame);
-    number_of_frames = number_of_frames + 1;
-end
-%}
-
-[rgb_value, cmap] = imread("Custom_Data/frames_single_fish/0003.png");
-[image_height, image_width] = size(rgb_value);
-
-%frame = readFrame(v);
-% Here we create the X and Y space
-% We want to make sure that the X and Y coordinates are
-% evenly spaced.
-X = ones(image_height, image_width); 
-for i = 1:image_width
-    X(:, i) = i;
-end
-Y = ones(image_height, image_width);
-for i = 1:image_height
-    Y(i,:) = i;
-end
-
-%normal = 255-rgb_value; % Inversed intensity
-normal = rgb_value;
-
-%mesh(X,Y,normal);
-%imshow(rgb_value);
-colorbar();
-histogram(normal);
+version https://git-lfs.github.com/spec/v1
+oid sha256:60aac071a75b9933c5449ce85c61fabcb754890d2b6d84617e308ee7f062a7b9
+size 760
